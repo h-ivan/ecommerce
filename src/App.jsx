d9bc659f8d691c97eadacb58d60ec99a3a21b1b1
Ivan Herman <h649477@gmail.com>
2022-07-13 23:42:44 -0300

    navbar v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import './App.scss'
+import './App.scss';
 import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App() {
 
-  return (
-    <div>
-        <Navbar/>
-      test
-    </div>
-  )
+    return (
+        <div className="app">
+            <Navbar/>
+            <h2 className="text-center">
+                hero section
+            </h2>
+            <a className="text-center block" href="https://www.figma.com/file/hvOVFRLQzaG36XL6B69tru/Ecommerce-Coderhouse">Link
+                to figma design</a>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,21 @@
-import './Navbar.scss'
+import './Navbar.scss';
+import {ReactComponent as CartIcon} from '../../assets/img/cart-icon.svg';
+import {ReactComponent as LoginIcon} from '../../assets/img/login-icon.svg';
 
 function Navbar() {
     return (
-        <div>
-            navbar
-        </div>
-    )
+        <header className="header container">
+            <h1 className="brand-name">
+                black & white
+            </h1>
+            <a href="#">
+                <LoginIcon className="menu-icon"/>
+            </a>
+            <a href="#">
+                <CartIcon className="menu-icon"/>
+            </a>
+        </header>
+    );
 }
 
 export default Navbar;
