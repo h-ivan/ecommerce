d4b88d9912731e70bbbba21310240860949ff738
ivan77n <h649477@gmail.com>
2022-07-13 22:07:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+function App() {
+
+  return (
+    <div>
+        <Navbar/>
+      test
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.scss'
+
+function Navbar() {
+    return (
+        <div>
+            navbar
+        </div>
+    )
+}
+
+export default Navbar;
